c139d36c0254e158a6d43960f15eb148bc759778
Badal-Kakade <badal.kakade@annalect.com>
2024-07-05 13:36:10 +0530

    update

---
+++ src/Admin/WelcomeScreen.js
@@ -0,0 +1,11 @@
+import React, {useRef} from 'react'
+import Login from '../Components/Login/Login';
+
+const WelcomeScreen = () => {
+    
+  return (
+    <Login />
+  )
+}
+
+export default WelcomeScreen
